7723c26a12077ba9f232fa1913b0cea5ce9271d4
migvasc <miguel.felipesilva@gmail.com>
2024-11-29 16:56:01 +0100

    fix git download url

--- pkgs/simgrid_3_36_custom/custom.nix
+++ pkgs/simgrid_3_36_custom/custom.nix
@@ -4,7 +4,8 @@ simgrid.overrideAttrs(oldAttrs: rec {
   version = oldAttrs.version + "-3.36_custom_vivaldi";
   rev = "3113891c1d11050c739ff5c0ef4692b2937b765c"; # The desired SimGrid commit.
   src = fetchurl {
-    url = "https://github.com/migvasc/custom_simgrid_3_36/-/archive/${rev}/simgrid-${rev}.tar.gz";
+    url = "https://api.github.com/repos/migvasc/custom_simgrid_3_36/tarball/${rev}";
+    
     sha256 = "sha256-DgU17ocCqWnvVDUNrngJhT/lq/z76GxHOA27c4f9z6o=";
   };
 })
