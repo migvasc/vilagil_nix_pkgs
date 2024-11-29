6a1104fe267b72121a92dcd5434d4a9242d0c1ae
migvasc <miguel.felipesilva@gmail.com>
2024-11-29 17:08:10 +0100

    fix sha simgrid custom

--- pkgs/simgrid_3_36_custom/custom.nix
+++ pkgs/simgrid_3_36_custom/custom.nix
@@ -5,6 +5,6 @@ simgrid.overrideAttrs(oldAttrs: rec {
   rev = "45ff8e53fb5128e61554714c900e14695df0cc36"; # The desired SimGrid commit.
   src = fetchurl {
     url = "https://gitlab.com/migvasc/custom_simgrid_336/-/archive/${rev}/simgrid-${rev}.tar.gz";
-    sha256 = "sha256-9wOpOvw+bUfXJpMYQdtkeBZ7nwo36iNq1Co/gkbSaq4=";
+    sha256 = "sha256-VWdnsq65ZtLkGpn3mU0agRECcZ3fsg+abmMjbiI/ySo=";
   };
 })
