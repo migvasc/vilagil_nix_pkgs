32b8f4cb419ab8c3f7903b30e959d9e67c26be7d
migvasc <miguel.felipesilva@gmail.com>
2024-11-29 13:26:28 +0100

    first commit

---
+++ default.nix
@@ -0,0 +1,17 @@
+{
+  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz) {}
+}:
+
+with pkgs;
+
+
+
+let
+  packages = rec {
+    simulationmain_custom_sg = callPackage ./pkgs/simulationmain { simgrid = custom_simgrid; };    
+    simgrid_3_36_custom = callPackage ./pkgs/simgrid_3_36_custom/custom.nix {};    
+inherit pkgs; # similar to `pkgs = pkgs;` This lets callers use the nixpkgs version defined in this file.
+
+};
+in
+  packages
