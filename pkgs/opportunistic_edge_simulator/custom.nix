8a804077fbf0f5b10dda5c4f716d9f23946a5473
migvasc <miguel.felipesilva@gmail.com>
2024-11-29 16:53:52 +0100

    fix sha version simulator

--- pkgs/opportunistic_edge_simulator/custom.nix
+++ pkgs/opportunistic_edge_simulator/custom.nix
@@ -6,7 +6,7 @@
   src = fetchgit {
     url = "https://github.com/migvasc/smart_city_cloud_edge_simulator";
     rev = "f332705084101f8ff05cf245643a834072ff5616";
-    sha256 = "sha256-Si5wmrUz4rrC29HOqtGPHTg/JGm3HDHLuk3ws4r/kok=";
+    sha256 = "sha256-jYxg7IUa0F1HGZnWmHID/HgLlB7zcJTMkJA6Bmg5PHo=";
   };
 
   buildInputs = [
