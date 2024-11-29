f010253f77eaa7aed9603f8ecfc8dd2a2842edfa
migvasc <miguel.felipesilva@gmail.com>
2024-11-29 14:08:01 +0100

    fix version nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 {
-  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/32b8f4cb419ab8c3f7903b30e959d9e67c26be7d.tar.gz) {}
+  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz) {}
 }:
 
 with pkgs;
