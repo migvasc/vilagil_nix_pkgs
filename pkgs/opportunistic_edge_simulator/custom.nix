8fafb51f0ac91cfc69b814b4870e8cd32b5683ca
migvasc <miguel.felipesilva@gmail.com>
2024-12-06 11:07:50 +0100

    updating simul pkg e068ff5c36044c1628e856121dd1f311ef74d619

--- pkgs/opportunistic_edge_simulator/custom.nix
+++ pkgs/opportunistic_edge_simulator/custom.nix
@@ -5,7 +5,7 @@
   version = "0.0.in-tuto";
   src = fetchgit {
     url = "https://github.com/migvasc/smart_city_cloud_edge_simulator";
-    rev = "e1c4a797faaa44d4e7ac55274e92fff03198f856";
+    rev = "e068ff5c36044c1628e856121dd1f311ef74d619";
     sha256 = "sha256-O3qGfNQzCIrY6L3J9maOnf2hCgfPLgJGHMdZiK7tf6Q=";
   };
 
