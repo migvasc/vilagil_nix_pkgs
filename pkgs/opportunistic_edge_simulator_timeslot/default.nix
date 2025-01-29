e54b7db2f4557dcae6f4abdec7ecfa7268f68a87
migvasc <miguel.felipesilva@gmail.com>
2025-01-29 13:47:21 +0100

    fix watt hour instead of joules

--- pkgs/opportunistic_edge_simulator_timeslot/default.nix
+++ pkgs/opportunistic_edge_simulator_timeslot/default.nix
@@ -5,8 +5,8 @@
   version = "0.0.in-tuto";
   src = fetchgit {
     url = "https://github.com/migvasc/smart_city_cloud_edge_simulator";
-    rev = "650f602a362ec40e41d12cb5f66622a19a1c0ba2";
-    sha256 = "sha256-qTsht9hf7w1VY/5/SAqvigJdwSejDX35/939uuhZIVo=";
+    rev = "0fed359f13a7902605b0714f26a2411c4b70c2c1";
+    sha256 = "sha256-JuZWJqKLSCDMAPhmaRnN47UMjvmzm6CyoGz7KFdkJi4=";
   };
 
   buildInputs = [
